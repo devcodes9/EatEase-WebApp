53c478920fa5ef982e5a56155f91379647f7c04f
Dev Samir Dalia <devdalia9@gmail.com>
2022-11-03 00:52:02 +0530

    Food List added

---
+++ client/eat-ease/src/components/foodList/FoodList.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './foodList.css'
+
+export const FoodList = () => {
+  return (
+    <div className='fList'>
+        <div className='fListItem'> 
+        <img src='https://www.nehascookbook.com/wp-content/uploads/2022/09/Gujarati-thali-WS-1.jpg' className='fListImg'></img> 
+        <div className='fListTitle'>
+            <h1>Gujarti</h1>
+            <h2>50 Kitchens</h2>
+        </div>
+        </div>
+        <div className='fListItem'> 
+        <img src='https://www.nehascookbook.com/wp-content/uploads/2022/09/Gujarati-thali-WS-1.jpg' className='fListImg'></img> 
+        <div className='fListTitle'>
+            <h1>Gujarti</h1>
+            <h2>50 Kitchens</h2>
+        </div>
+        </div>
+        <div className='fListItem'> 
+        <img src='https://www.sodhatravel.com/hubfs/Thali.jpg' className='fListImg'></img> 
+        <div className='fListTitle'>
+            <h1>North Indian</h1>
+            <h2>27 Kitchens</h2>
+        </div>
+        </div>
+        <div className='fListItem'> 
+        <img src='https://www.sodhatravel.com/hubfs/Thali.jpg' className='fListImg'></img> 
+        <div className='fListTitle'>
+            <h1>North Indian</h1>
+            <h2>27 Kitchens</h2>
+        </div>
+        </div>
+        <div className='fListItem'> 
+        <img src='https://www.sodhatravel.com/hubfs/Thali.jpg' className='fListImg'></img> 
+        <div className='fListTitle'>
+            <h1>North Indian</h1>
+            <h2>27 Kitchens</h2>
+        </div>
+        </div>
+        <div className='fListItem'> 
+        <img src='https://www.sodhatravel.com/hubfs/Thali.jpg' className='fListImg'></img> 
+        <div className='fListTitle'>
+            <h1>North Indian</h1>
+            <h2>27 Kitchens</h2>
+        </div>
+        </div>
+    </div>
+  )
+}
--- client/eat-ease/src/pages/home/Home.jsx
+++ client/eat-ease/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css"
 import {Navbar} from "../../components/navbar/Navbar.jsx"
 import { Header } from '../../components/header/Header'
 import { Featured } from '../../components/featured/Featured'
+import { FoodList } from '../../components/foodList/FoodList'
 
 export const Home = () => {
     return (
@@ -10,7 +11,9 @@ export const Home = () => {
             <Navbar />
             <Header />
             <div className='homeContainer'>
-            <Featured />
+            <h1 className='homeTitle'>Browse by Cuisine</h1>
+            <FoodList />
+            <Featured />  
             </div>
         </div>
     )
