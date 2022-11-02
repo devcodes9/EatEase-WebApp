3b6ede2cf6fba4ff8d38931952e7a38ad2811ffd
Dev Samir Dalia <devdalia9@gmail.com>
2022-11-03 00:07:00 +0530

    Kitchen List added

--- client/eat-ease/src/pages/home/Home.jsx
+++ client/eat-ease/src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./home.css"
 import {Navbar} from "../../components/navbar/Navbar.jsx"
 import { Header } from '../../components/header/Header'
+import { Featured } from '../../components/featured/Featured'
 
 export const Home = () => {
     return (
         <div>
             <Navbar />
             <Header />
+            <div className='homeContainer'>
+            <Featured />
+            </div>
         </div>
     )
 }
