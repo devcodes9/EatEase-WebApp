e36477f9a2fdc8c4ff3444482a7461bb0ed38dba
Dev Samir Dalia <devdalia9@gmail.com>
2023-01-14 23:54:45 +0530

    use Fetch Added

--- client/eat-ease/src/components/featured/Featured.jsx
+++ client/eat-ease/src/components/featured/Featured.jsx
@@ -1,44 +1,47 @@
 import React from 'react'
 import './featured.css';
+import { useFetch } from '../../hooks/useFetch'; 
 
 const Featured = () => {
-  return (
-    <div className='featured'>
-    <div className="container"> 
-        <div className="card text-bg-dark mb-3">
-            <img src="https://images.unsplash.com/photo-1616170687910-2edb17b633d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="card-img" alt="..." ></img>
-            <div className="card-img-overlay">
-                <h5 className="card-title">Apna Kitchen</h5>
-                <p className="card-text">Delicious Apne vale Plans</p>
-                <a href="#" className="btn" style={{backgroundColor: "#DD5642"}}>Subscribe</a>
-                <p className="card-text"><small>30 Subscribed</small></p>
+    const {data, loading, error} = useFetch("/kitchens/countByKitchen?cities=Gandhinagar,Surat,Vijapur")
+    console.log(data);
+    return (
+        <div className='featured'>
+            <div className="container">
+                <div className="card text-bg-dark mb-3">
+                    <img src="https://images.unsplash.com/photo-1616170687910-2edb17b633d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="card-img" alt="..." ></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Apna Kitchen</h5>
+                        <p className="card-text">Delicious Apne vale Plans</p>
+                        <a href="#" className="btn" style={{ backgroundColor: "#DD5642" }}>Subscribe</a>
+                        <p className="card-text"><small>30 Subscribed</small></p>
+                    </div>
+                </div>
             </div>
+            <div className="container">
+                <div className="card text-bg-dark mb-3">
+                    <img src="https://images.unsplash.com/photo-1585672840563-f2af2ced55c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="card-img" alt="..." ></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Mumma's best</h5>
+                        <p className="card-text">Quality & Reasonable</p>
+                        <a href="#" className="btn" style={{ backgroundColor: "#DD5642" }}>Subscribe</a>
+                        <p className="card-text"><small>28 Subscribed</small></p>
+                    </div>
+                </div>
             </div>
-    </div>
-    <div className="container">
-        <div className="card text-bg-dark mb-3">
-        <img src="https://images.unsplash.com/photo-1585672840563-f2af2ced55c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="card-img" alt="..." ></img>
-        <div className="card-img-overlay">
-            <h5 className="card-title">Mumma's best</h5>
-            <p className="card-text">Quality & Reasonable</p>
-            <a href="#" className="btn" style={{backgroundColor: "#DD5642"}}>Subscribe</a>
-            <p className="card-text"><small>28 Subscribed</small></p>
+            <div className="container">
+                <div className="card text-bg-dark mb-3">
+                    <img src="https://images.pexels.com/photos/941869/pexels-photo-941869.jpeg" className="card-img" alt="..." ></img>
+                    <div className="card-img-overlay">
+                        <h5 className="card-title">Khana Khazana</h5>
+                        <p className="card-text">South Indian's Vadakkam</p>
+                        <a href="#" className="btn" style={{ backgroundColor: "#DD5642" }}>Subscribe</a>
+                        <p className="card-text"><small>18 Subscribed</small></p>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-    <div className="container">
-    <div className="card text-bg-dark mb-3">
-        <img src="https://images.pexels.com/photos/941869/pexels-photo-941869.jpeg" className="card-img" alt="..." ></img>
-        <div className="card-img-overlay">
-            <h5 className="card-title">Khana Khazana</h5>
-            <p className="card-text">South Indian's Vadakkam</p>
-            <a href="#" className="btn" style={{backgroundColor: "#DD5642"}}>Subscribe</a>
-            <p className="card-text"><small>18 Subscribed</small></p>
-        </div>
-        </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Featured
---
+++ client/eat-ease/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState([false]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await axios.get(url);
+                setData(res.data);
+            } catch (err) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [url]);
+
+    const reFetch = async () => {
+        setLoading(true);
+        try {
+            const res = await axios.get(url);
+            setData(res.data);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    return {data, loading, error, reFetch};
+};
+
