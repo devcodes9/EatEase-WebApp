d4c736a706ba4bb179cda616e017f2b0d03caa81
Dev Samir Dalia <devdalia9@gmail.com>
2023-01-28 10:59:13 +0530

    Reverting to 39c9539

---
+++ client/eat-ease/src/context/SearchContext.js
@@ -0,0 +1,33 @@
+import {useReducer} from 'react'
+import { createContext } from 'react';
+
+const INITIAL_STATE = {
+    food: undefined,
+    city: undefined,
+    dates: [],
+};
+
+
+export const SearchContext = createContext(INITIAL_STATE)
+
+const SearchReducer = (state, action) => {
+    switch (action.type) {
+        case "NEW_SEARCH":
+            return action.payload;
+        case "RESET_SEARCH":
+            return INITIAL_STATE;
+        default:
+            return state;
+    }
+};
+
+
+export const SearchContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(SearchReducer, INITIAL_STATE);
+
+    return(
+        <SearchContext.Provider value = {{food: state.food, city: state.city, dates: state.dates, dispatch}}>
+            {children}
+        </SearchContext.Provider>
+    )
+}
