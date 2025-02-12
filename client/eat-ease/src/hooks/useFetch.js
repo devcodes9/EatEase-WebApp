4bcae73568a917c98bb6745d04b75f3316d3d136
Dev Dalia <devdalia9@gmail.com>
2025-02-12 23:04:58 +0530

    Update useFetch.js

--- client/eat-ease/src/hooks/useFetch.js
+++ client/eat-ease/src/hooks/useFetch.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-  const URL = `${process.env.REACT_APP_API_URL}${url}`;
+  console.log("BACKEND URL:", process.env.REACT_APP_API_URL);
+  const URL = `${
+    process.env.REACT_APP_API_URL || "https://eat-ease-backend.onrender.com"
+  }${url}`;
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState([false]);
