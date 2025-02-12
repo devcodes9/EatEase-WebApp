afa3f86adfd06f7f5db7c89cd4481a4d67b12d28
Dev Dalia <devdalia9@gmail.com>
2025-02-12 22:22:29 +0530

    from env

--- api/index.js
+++ api/index.js
@@ -1,44 +1,44 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth.js")
-const usersRoute = require("./routes/users.js")
-const kitchensRoute = require("./routes/kitchens.js")
-const plansRoute = require("./routes/plans.js")
+const authRoute = require("./routes/auth.js");
+const usersRoute = require("./routes/users.js");
+const kitchensRoute = require("./routes/kitchens.js");
+const plansRoute = require("./routes/plans.js");
 const cookieParser = require("cookie-parser");
-var cors = require('cors')
+var cors = require("cors");
 
 const app = express();
 dotenv.config();
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    })
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
 );
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO);
-        console.log("Connected to Database!");
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to Database!");
+  } catch (error) {
+    throw error;
+  }
+};
 
 mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB disconnected");
+  console.log("MongoDB disconnected");
 });
 
 mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected");
+  console.log("MongoDB connected");
 });
 
 //midllewares
@@ -50,6 +50,6 @@ app.use("/api/kitchens", kitchensRoute);
 app.use("/api/plans", plansRoute);
 
 app.listen(8080, () => {
-    connect();
-    console.log("Listening on port 8080!");
-})
+  connect();
+  console.log("Listening on port 8080!");
+});
